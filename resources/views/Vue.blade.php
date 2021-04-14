b5327c4f1c09268996faadacaa815643b1c5858d
Sweets <Sweets@users.noreply.github.com>
2021-04-14 06:26:29 +0000

    Blade template for Vue

---
+++ resources/views/Vue.blade.php
@@ -0,0 +1,11 @@
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>My Test</title>
+	<link href="/css/app.css" type="text/css" rel="stylesheet"></link>
+</head>
+<body>
+	<app id="vue"></app>
+	<script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
