372093579af7576672c9a3c89d4692f25daa8e8d
Sweets <Sweets@users.noreply.github.com>
2021-04-14 06:25:21 +0000

    Work on frontend with Vue

--- resources/views/App.vue
+++ resources/views/App.vue
@@ -1,9 +1,64 @@
 <template>
 	<div>
-		<span>My Vue.js + Laravel Application!</span>
-		<router-view></router-view>
+		<header>
+			<template v-if="logged_in == false">
+				<a href="javascript:;" v-on:click="show_login_modal">Login</a>
+				<a href="javascript:;" v-on:click="show_registration_modal">Register</a>
+			</template>
+			<template v-else>
+				Hello, User! <a href="javascript:;" v-on:click="user_logout">Logout</a>
+			</template>
+		</header>
+		<router-view />
+		<footer>
+			&copy; MMXXI
+		</footer>
 	</div>
 </template>
 <script>
-    export default {};
+	import axios from 'axios';
+	import Swal from 'sweetalert2';
+
+	import { Modal } from '../js/modal.js';
+	import Login from '../views/modals/Login.vue';
+	import Register from '../views/modals/Register.vue';
+
+    export default {
+		data: function() {
+			return {
+				get logged_in() {
+					return localStorage.getItem('ApiToken') != null;
+				},
+				RegistrationModal: null,
+				LoginModal: null
+			};
+		},
+		methods: {
+			show_registration_modal: function() {
+				let context = this;
+				Modal(this, Register).fire(function(Component) {
+					Component.submit_registration(function() {
+						context.$forceUpdate();
+					});
+				});
+			},
+			show_login_modal: function() {
+				let context = this;
+				Modal(this, Login).fire(function(Component) {
+					Component.submit_login(function() {
+						context.$forceUpdate();
+					});
+				});
+			},
+			user_logout: function() {
+				localStorage.removeItem('ApiToken');
+				this.$forceUpdate();
+			}
+		},
+		mounted: function() {
+/*			let _vm = this.$options._base;
+			this.RegistrationModal = _vm.extend.apply(_vm, [Register]);
+			this.LoginModal = _vm.extend.apply(_vm, [Login]);*/
+		}
+	};
 </script>
